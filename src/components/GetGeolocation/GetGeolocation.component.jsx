160d5aa0dcb981dd631e29c92f35ab3c9656ec87
isissi <isissi_525@hotmail.com>
2022-06-10 13:05:49 -0600

    add feature get geolocation

---
+++ src/components/GetGeolocation/GetGeolocation.component.jsx
@@ -0,0 +1,42 @@
+import { Button } from "antd";
+import Skeleton from 'react-loading-skeleton'
+import { useState } from "react";
+
+import "./GetGeolocation.styles.scss";
+import "antd/dist/antd.css";
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const GetGeolocation = (props) => {
+  const { geolocation, setGeolocation } = props;
+
+  const [ buttonClicked, setButtonClicked ] = useState(false);
+
+  const getCordinate = () => {
+    setButtonClicked(true);
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setGeolocation({latitude: position.coords.latitude, longitude: position.coords.longitude})
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+
+  const cordinate = !geolocation ? <Skeleton width={450} height={30}/> : `Latitude: ` + geolocation.latitude + `, Longitude: ` + geolocation.longitude;
+
+  return (
+    <div id="geo-button-container">
+      {!buttonClicked 
+        ?
+        <h2>Get your geolocation from browser ↓</h2> 
+        :
+        <h2>{cordinate}</h2> 
+      }
+      
+      <Button type="primary" id="geo-button" onClick={getCordinate}>
+        Get Geolocation
+      </Button>
+    </div>
+  )
+}
+
+export default GetGeolocation;
