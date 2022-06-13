5e587eccdb74046735dff270e67c11cd51da5380
isissi <isissi_525@hotmail.com>
2022-06-12 19:34:54 -0600

    add function getTimezone

---
+++ src/helpers/getTimezone.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const getTimezone = (location) => {
+  const currentDate = new Date();
+  const timestamp = currentDate.getTime();
+  
+  let promise = axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat}%2C${location.lng}&timestamp=1331161200&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`);
+
+  return(
+  promise
+  .then(res => {return res})
+  .catch((error) => console.log("error: " + error))
+  )
+}
+
+export default getTimezone;
